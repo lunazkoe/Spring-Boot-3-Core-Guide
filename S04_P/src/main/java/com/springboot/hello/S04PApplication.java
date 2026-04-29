78d81eaae405f610fcba065ef89f2f80ff2247e4
lunazkoe <lunazkoe@gmail.com>
2026-04-30 02:30:31 +0900

    [FINISH] S04_P

---
+++ S04_P/src/main/java/com/springboot/hello/S04PApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04PApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S04PApplication.class, args);
+    }
+
+}
