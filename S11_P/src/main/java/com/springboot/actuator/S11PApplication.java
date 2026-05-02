b36d99d7b513fccc855137df0ac37b0268494c3c
lunazkoe <lunazkoe@gmail.com>
2026-05-02 19:28:05 +0900

    [INIT] S11_P

---
+++ S11_P/src/main/java/com/springboot/actuator/S11PApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S11PApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S11PApplication.class, args);
+    }
+
+}
